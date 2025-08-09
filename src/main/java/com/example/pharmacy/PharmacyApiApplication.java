8b15dcebf49793c28abfc24b77d2eb5799d94139
Kaleab <tadewoskaleab@gmail.com>
2025-08-09 16:29:47 -0500

    Initialize Project

---
+++ src/main/java/com/example/pharmacy/PharmacyApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.pharmacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacyApiApplication.class, args);
+	}
+
+}
